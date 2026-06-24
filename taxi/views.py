8386da68fdf6071f1318c3b2391d4d5cf3b99081
Mykola Mosiienko <koljamos@gmail.com>
2026-06-24 08:27:23 +0300

    fixed num_visits, better styles for sidebar

--- taxi/views.py
+++ taxi/views.py
@@ -11,8 +11,8 @@ def index(request):
     num_drivers = Driver.objects.count()
     num_cars = Car.objects.count()
     num_manufacturers = Manufacturer.objects.count()
-    num_visits = request.session.get("num_visits", 0) + 1
-    request.session["num_visits"] = num_visits
+    num_visits = request.session.get("num_visits", 1)
+    request.session["num_visits"] = num_visits + 1
 
     context = {
         "num_drivers": num_drivers,
